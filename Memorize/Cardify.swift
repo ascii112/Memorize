83e514da1d91f2f5234a1a518decd5644cd98b2e
peerawat yoouthong <>
2020-12-11 11:09:32 +0700

    add animation

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -7,19 +7,32 @@
 //
 
 import SwiftUI
-struct Cardify: ViewModifier{
-    var isFaceUp: Bool = false
+struct Cardify: AnimatableModifier{
+    var rotation: Double
+    init(isFaceUp: Bool) {
+        rotation  = isFaceUp ? 0 : 180
+    }
+    var isFaceUp: Bool{
+        rotation < 90
+    }
     
+    var animatableData: Double{
+        get {return rotation}
+        set {rotation = newValue}
+    }
     func body(content: Content) -> some View {
         ZStack{
-            if isFaceUp{
+            Group{
                 RoundedRectangle(cornerRadius: self.cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: self.cornerRadius).stroke(lineWidth: self.edgeLineWidth)
                 content
-            }else{
+            }.opacity(isFaceUp ? 1 : 0)
                 RoundedRectangle(cornerRadius: self.cornerRadius).fill()
-            }
+                    .opacity(isFaceUp ? 0 : 1)
         }
+        .rotation3DEffect(
+            Angle.degrees(rotation),
+            axis: (0,1,0))
     }
     private let cornerRadius: CGFloat = 10.0
     private let edgeLineWidth: CGFloat = 3
